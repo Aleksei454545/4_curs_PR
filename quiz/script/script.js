48e284b0679e7fe9ad5292e651012211da7cf669
Aleksei Vaseiko <vaseikoalex@gmail.com>
2025-12-24 13:04:57 +0200

    3

--- quiz/script/script.js
+++ quiz/script/script.js
@@ -1,11 +1,92 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    'use strict'
+
     const btnOpenModal = document.querySelector("#btnOpenModal");
     const modalBlock = document.querySelector("#modalBlock");
     const closeModal = document.querySelector("#closeModal");
     const questionTitle = document.querySelector("#question");
     const formAnswers = document.querySelector("#formAnswers");
+    const prevButton = document.querySelector("#prev");
+    const nextButton = document.querySelector("#next");
+
     
+    const questions = [
+    {
+        question: "Якого кольору бургер?",
+        answers: [
+            {
+                title: 'Стандарт',
+                url: './image/burger.png'
+            },
+            {
+                title: 'Чорний',
+                url: './image/burgerBlack.png'
+            }
+        ],
+        type: 'radio'
+    },
+    {
+        question: "З якого м'яса котлета?",
+        answers: [
+            {
+                title: 'Курка',
+                url: './image/chickenMeat.png'
+            },
+            {
+                title: 'Яловичина',
+                url: './image/beefMeat.png'
+            },
+            {
+                title: 'Свинина',
+                url: './image/porkMeat.png'
+            }
+        ],
+        type: 'radio'
+    },
+    {
+        question: "Додаткові інгредієнти ?",
+        answers: [
+            {
+                title: 'Помідор',
+                url: './image/tomato.png'
+            },
+            {
+                title: 'Огірок',
+                url: './image/cucumber.png'
+            },
+            {
+                title: 'Салат',
+                url: './image/salad.png'
+            },
+            {
+                title: 'Цибуля',
+                url: './image/onion.png'
+            }
+        ],
+        type: 'checkbox'
+    },
+    {
+        question: "Додати соус?",
+        answers: [
+            {
+                title: 'Часниковий',
+                url: './image/sauce1.png'
+            },
+            {
+                title: 'Томатний',
+                url: './image/sauce2.png'
+            },
+            {
+                title: 'Гірчичний',
+                url: './image/sauce3.png'
+            }
+        ],
+        type: 'radio'
+    }
+];
+
     btnOpenModal.addEventListener("click",()=>{
+        console.log("click");
         modalBlock.classList.add("d-block");   
         playTest();
 
@@ -15,31 +96,64 @@ document.addEventListener("DOMContentLoaded",()=>{
         modalBlock.classList.remove("d-block");   
 
     })
-    const playTest = function() {
+    const playTest = () => {
+    let numberQuestion = 0;
 
-        const burgerName1 = "Стандарт";
-        const burgerImg1 = "./image/burger.png";
+    const checkButtons = (index) => {
 
-        const burgerName2 = "Чорний";
-        const burgerImg2 = "./image/burgerBlack.png";
+        if (index === 0) {
+            prevButton.classList.add('d-none');
+        } else {
+            prevButton.classList.remove('d-none');
+        }
 
-        questionTitle.textContent = "Якого кольору бургер бажаєте?";
-
-        formAnswers.innerHTML = `
-            <div class="answers-item d-flex flex-column">
-                <input type="radio" id="answerItem1" name="answer" class="d-none">
-                <label for="answerItem1" class="d-flex flex-column justify-content-between">
-                  <img class="answerImg" src="${burgerImg1}" alt="burger">
-                  <span>${burgerName1}</span>
-                </label>
-              </div>
-              <div class="answers-item d-flex justify-content-center">
-                <input type="radio" id="answerItem2" name="answer" class="d-none">
-                <label for="answerItem2" class="d-flex flex-column justify-content-between">
-                  <img class="answerImg" src="${burgerImg2}" alt="burger">
-                  <span>${burgerName2}</span>
-                </label>
-              </div>
-        `;
+        if (index === questions.length - 1) {
+            nextButton.classList.add('d-none');
+        } else {
+            nextButton.classList.remove('d-none');
+        }
     };
-});
+
+    const renderAnswers = (index) => {
+        questions[index].answers.forEach((answer) => {
+            const answerItem = document.createElement("div");
+            answerItem.classList.add("answers-item", "d-flex", "flex-column");
+
+            
+            answerItem.innerHTML = `
+                <input type="${questions[index].type}" id="${answer.title}" name="answer" class="d-none">
+                <label for="${answer.title}" class="d-flex flex-column justify-content-between">
+                    <img class="answerImg" src="${answer.url}" alt="${answer.title}">
+                    <span>${answer.title}</span>
+                </label>
+            `;
+            formAnswers.appendChild(answerItem);
+        });
+    };
+
+    const renderQuestions = (indexQuestion) => {
+        formAnswers.innerHTML = "";
+        questionTitle.textContent = `${questions[indexQuestion].question}`;
+        
+        renderAnswers(indexQuestion);
+        checkButtons(indexQuestion); 
+    };
+
+    renderQuestions(numberQuestion);
+
+    prevButton.onclick = () => {
+        if (numberQuestion > 0) {
+            numberQuestion--;
+            renderQuestions(numberQuestion);
+        }
+    };
+
+    nextButton.onclick = () => {
+        if (numberQuestion < questions.length - 1) {
+            numberQuestion++;
+            renderQuestions(numberQuestion);
+        }
+    };
+};
+})
+    
